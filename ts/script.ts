437f73c65149ff62cc6006a7f50820c1b9db70c8
Aleksandra <45710112+KHiMAeRA05@users.noreply.github.com>
2022-06-18 17:45:59 +0300

    Update script.ts

--- ts/script.ts
+++ ts/script.ts
@@ -1,21 +1,21 @@
 const getWeather = (event: MouseEvent) => {
 
-  let weather:WData|WError|WLoading = {loading: true}
+  let weather: WData | WError | WLoading = {loading: true}
   const resWeather = document.getElementById("get-weather");
+
 
   fetch("https://weatherdbi.herokuapp.com/data/weather/" + "Innopolis")
     .then((res) => res.text())
-    .then((res) => JSON.parse(res) as WData|WError)
+    .then((res) => JSON.parse(res) as WData | WError)
     .then((res) => {
       weather = res;
-      if((weather as WData).currentConditions) {
+      if ((weather as WData).currentConditions) {
         resWeather.innerText = (weather as WData).currentConditions.temp.c.toString();
-        resWeather.innerText += '℃ | ' + (weather as WData).currentConditions.temp.f +'℉\nInnopolis,\nRepublic of Tatarstan';
+        resWeather.innerText += '℃ | ' + (weather as WData).currentConditions.temp.f + '℉\nInnopolis,\nRepublic of Tatarstan';
         console.log(resWeather);
-      }
-      else if ((weather as WError).status) resWeather.innerText = "There is no such a location";
+      } else if ((weather as WError).status) resWeather.innerText = "There is no such a location";
     });
-  if((weather as WLoading).loading) resWeather.innerText = "Please Stand By 💿";
+  if ((weather as WLoading).loading) resWeather.innerText = "Please Stand By 💿";
 }
 
 const main = () => {
@@ -29,6 +29,7 @@ window.onload = main;
 interface WError {
   status: string;
 }
+
 interface WLoading {
   loading: boolean;
 }
