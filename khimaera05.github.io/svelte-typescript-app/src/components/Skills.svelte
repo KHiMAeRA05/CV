18ecca12a7d9e056cc70900ff285fac1d90deb98
KHiMAeRA05 <Khaios@mail.ru>
2022-06-25 16:32:52 +0300

    Svelte is working

---
+++ khimaera05.github.io/svelte-typescript-app/src/components/Skills.svelte
@@ -0,0 +1,38 @@
+<!-- Skills-->
+<section class="cv-section" id="skills">
+    <div>
+        <h2 class="cv-section-headings" style="background: #00CFC1;">Skills</h2>
+
+        <div class="cv-section-text">
+            <div class="row-section">
+                <div class="column-section">
+                    <h3 class="mb-0">Hard skills</h3>
+
+                    <li class="mb-1">Competitive programming: C++, Java</li>
+                    <li class="mb-1">Machine Learning projects: Python, Jupyter notebook</li>
+                    <li class="mb-1">Frontend web-dev projects: HTML, CSS, JavaScript</li>
+                    <li class="mb-1">GitHub</li>
+                </div>
+            </div>
+
+            <div class="row-section">
+                <div class="column-section">
+                    <h3 class="mb-0">Soft skills</h3>
+                    <li class="mb-1">Proactive</li>
+                    <li class="mb-1">strong emotional intelligence</li>
+
+                </div>
+            </div>
+            <div class="row-section">
+                <div class="column-section">
+                    <h3 class="mb-0">Languages</h3>
+
+                    <li class="mb-1">Russian, native</li>
+                    <li class="mb-1">English, B2+ level</li>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+</section>
