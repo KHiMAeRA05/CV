efef8ecc4b1f5799a40f29fadf66c3681d26a883
Aleksandra <45710112+KHiMAeRA05@users.noreply.github.com>
2022-07-04 16:16:38 +0300

    Add files via upload

---
+++ khimaera05.github.io/svelte-typescript-app/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import '/public/global.css'
+    import API from './components/API.svelte';
+    import Header from './components/Header.svelte'
+    import Home from './components/Home.svelte'
+    import Education from "./components/Education.svelte";
+    import Skills from "./components/Skills.svelte";
+    import Interests from "./components/Interests.svelte";
+    import Awards from "./components/Awards.svelte";
+    import Footer from "./components/Footer.svelte";
+</script>
+
+<body>
+    <Header/>
+    <main>
+        <API/>
+        <hr style="border-top: 1px solid #333333;">
+        <Home/>
+        <hr style="border-top: 1px solid #333333;">
+        <Education/>
+        <hr style="border-top: 1px solid #333333;">
+        <Skills/>
+        <hr style="border-top: 1px solid #333333;">
+        <Interests/>
+        <hr style="border-top: 1px solid #333333;">
+        <Awards/>
+        <hr style="border-top: 1px solid #333333;">
+    </main>
+    <Footer/>
+
+</body>
---
+++ khimaera05.github.io/svelte-typescript-app/src/components/API.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+    interface WError {
+        status: string;
+    }
+
+    interface WLoading {
+        loading: boolean;
+    }
+
+    interface WData {
+        region: string;
+        currentConditions: {
+            temp: {
+                c: number;
+                f: number;
+            }
+        }
+    }
+
+    const getWeather = (event: MouseEvent) => {
+
+        let weather: WData | WError | WLoading = {loading: true}
+        const resWeather = document.getElementById("get-weather");
+
+
+        fetch("https://weatherdbi.herokuapp.com/data/weather/" + "Innopolis")
+            .then((res) => {
+                if (res.ok) {
+                    console.log(res);
+                    res.text();
+                }
+                else
+                    return res.json();
+            })
+            .then((res) => JSON.parse(res) as WData | WError)
+            .then((res) => {
+                weather = res;
+                if ((weather as WData).currentConditions) {
+                    resWeather.innerText = (weather as WData).currentConditions.temp.c.toString();
+                    resWeather.innerText += '℃ | ' + (weather as WData).currentConditions.temp.f + '℉\nInnopolis,\nRepublic of Tatarstan';
+                    console.log(resWeather);
+                } else if ((weather as WError).status) resWeather.innerText = "There is no such a location";
+            })
+            .catch(error => {
+                resWeather.innerText = `Error: \n${error}\n\n Looks like site isn't loading`;
+                console.error('There was an error!', error);
+            });
+        console.log(weather);
+        if ((weather as WLoading).loading) resWeather.innerText = "Please Stand By 💿";
+
+    }
+
+    const main = () => {
+        const update = document.getElementById("update-btn");
+        console.log(update);
+        update.addEventListener("click", getWeather);
+    }
+
+    window.onload = main;
+</script>
+
+<section class="cv-section" id="api">
+    <h2 class="cv-section-headings" style="color: #f0f0f0; background: #333333;">API</h2>
+    <div class="row container">
+        <div class="column api-left">
+
+            <div id="get-weather" class="weather-style">
+                <p class="mb-1"> What's the weather like today?</p>
+            </div>
+
+        </div>
+        <div class="column api-right">
+            <h3 class="mb-0" style="color: #333333;">Get the weather</h3>
+            <div class="cv-section-text">
+                <p class="mb-1" id="id1">Click the button to get the weather</p>
+
+                <button type="button" class="fill" id="update-btn">Update</button>
+
+            </div>
+
+        </div>
+    </div>
+</section>
