85424cc8ef420cc9127233b8e2789945e5f56217
KHiMAeRA05 <Khaios@mail.ru>
2022-07-05 11:38:02 +0300

    Deploy

---
+++ khimaera/src/routes/Footer.svelte
@@ -0,0 +1,79 @@
+<script>
+	let telegram = '/telegram.png';
+	let github = '/github.png';
+</script>
+
+<footer id="contact">
+	<div>
+		<h2 style="text-align: center; font-size: 4vw">Contact Me</h2>
+	</div>
+	<div class="row container">
+		<div class="column contactme-left">
+			<!--div class="phone">
+              <p><span class="label-left">Phone:</span><a href="tel://79617425228"> +7 (961) 742-52-28</a></p>
+            </div-->
+			<div class="email">
+				<p>
+					<span class="label-left">Email:</span><a href="mailto:a.voronova@innopolis.university">
+						a.voronova@innopolis.university</a
+					>
+				</p>
+			</div>
+			<div class="telegram">
+				<a href="https://t.me/Himera_05">
+					<img
+						border="0"
+						alt="github"
+						src={telegram}
+						style="max-width: 100px; max-height: 100px; float: left; margin: 20px"
+					/>
+				</a>
+			</div>
+			<div class="git">
+				<a href="https://github.com/KHiMAeRA05">
+					<img
+						border="0"
+						alt="github"
+						src={github}
+						style="max-width: 100px; max-height: 100px; float: left; margin: 20px"
+					/>
+				</a>
+			</div>
+		</div>
+		<div class="column contactme-right">
+			<form method="post" id="contactForm">
+				<div class="contactForm-name">
+					<label class="label" for="name">Full Name</label>
+					<input class="form-control" type="text" id="name" name="name" placeholder="Name" />
+				</div>
+				<div class="contactForm-email">
+					<label class="label" for="email">Email</label>
+					<input class="form-control" type="text" id="email" name="email" placeholder="Email" />
+				</div>
+				<div class="contactForm-subject">
+					<label class="label" for="subject">Subject</label>
+					<input
+						class="form-control"
+						type="text"
+						id="subject"
+						name="subject"
+						placeholder="Subject"
+					/>
+				</div>
+				<div class="contactForm-message">
+					<label class="label" for="message">Message</label>
+					<textarea
+						name="message"
+						class="form-control"
+						id="message"
+						placeholder="Message"
+						style="height: 112px;"
+					/>
+				</div>
+				<div class="contactForm-input">
+					<input type="submit" class="send-message" value="Send Message" />
+				</div>
+			</form>
+		</div>
+	</div>
+</footer>
--- khimaera/src/routes/index.svelte
+++ khimaera/src/routes/index.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import './items/global.css';
+	import API from './API.svelte';
+	import Header from './Header.svelte';
+	import Home from './Home.svelte';
+	import Education from './Education.svelte';
+	import Skills from './Skills.svelte';
+	import Interests from './Interests.svelte';
+	import Awards from './Awards.svelte';
+	import Footer from './Footer.svelte';
+</script>
+
+<body>
+	<Header />
+	<main>
+		<API />
+		<hr style="border-top: 1px solid #333333;" />
+		<Home />
+		<hr style="border-top: 1px solid #333333;" />
+		<Education />
+		<hr style="border-top: 1px solid #333333;" />
+		<Skills />
+		<hr style="border-top: 1px solid #333333;" />
+		<Interests />
+		<hr style="border-top: 1px solid #333333;" />
+		<Awards />
+		<hr style="border-top: 1px solid #333333;" />
+	</main>
+	<Footer />
+</body>
